2c202f7358923004ee57c3de0303da4853255bf2
hazeycode <22148308+hazeycode@users.noreply.github.com>
2025-08-31 15:17:20 +0100

    Zig 0.15.x compat.

--- build.zig
+++ build.zig
@@ -4,17 +4,15 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    _ = b.addModule("root", .{
-        .root_source_file = b.path("src/zjobs.zig"),
-    });
-
     const test_step = b.step("test", "Run zjobs tests");
 
     const tests = b.addTest(.{
         .name = "zjobs-tests",
-        .root_source_file = b.path("src/zjobs.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.addModule("root", .{
+            .root_source_file = b.path("src/zjobs.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     b.installArtifact(tests);
 
